4cd53b7d958f3534d70f8b8e41734ec2b7e97b3d
angga <anggaairulr@gmail.com>
2022-03-23 21:51:46 +0700

    inisialisasi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taksi taxi = new Taksi();
+
+            taxi.DriverName = "ANGGA";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
